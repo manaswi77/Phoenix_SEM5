e605b229befc2403a12d2010cfa3da1bc6593b5a
manaswi77 <devekarmanaswi@gmail.com>
2024-10-09 14:46:24 +0530

    SOS button dev started

--- myApp/src/components/Security_Screen_Components/SOSButtonScreen.tsx
+++ myApp/src/components/Security_Screen_Components/SOSButtonScreen.tsx
@@ -1,14 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, BackHandler } from "react-native";
+import React, { useEffect } from "react";
+import { AppDispatch, RootState } from "../../store/store";
+import { useDispatch, useSelector } from "react-redux";
+import { setCurrentFeature } from "../../contexts/securityFeatureSlice";
 
 const SOSButtonScreen = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const isSOSButtonEnabled = useSelector(
+    (state: RootState) => state.securityFeature.isSOSButtonEnabled
+  );
+
+  useEffect(() => {
+    const backAction = () => {
+      dispatch(setCurrentFeature("features"));
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => backHandler.remove();
+  }, [dispatch]);
   return (
     <View>
       <Text>SOSButtonScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default SOSButtonScreen
+export default SOSButtonScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
